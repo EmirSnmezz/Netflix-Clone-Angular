60f65e206c18e2df6d25aa65cac7681608f0c5c3
emir <emircan_snmez@outlook.com>
2024-06-04 04:10:52 +0300

    main page %50 completed

--- Netflix-Clone/src/app/main/main.component.ts
+++ Netflix-Clone/src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button'; 
+import {MatInputModule} from '@angular/material/input'; 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule,MatInputModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
