2e1184ab4caf7c054aa4e1b692d1023b735df7f1
emir <emircan_snmez@outlook.com>
2024-06-08 21:29:58 +0300

    footer2 and login page created.

--- Netflix-Clone/src/app/login-page/login-page.component.ts
+++ Netflix-Clone/src/app/login-page/login-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input'; 
 import { MatButton } from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [MatFormFieldModule,MatInputModule,MatButton],
+  imports: [MatFormFieldModule,MatInputModule,MatButton,MatCheckboxModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
--- Netflix-Clone/src/app/main/main.component.ts
+++ Netflix-Clone/src/app/main/main.component.ts
@@ -3,10 +3,11 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input'; 
 import {CdkAccordionModule} from '@angular/cdk/accordion';
 import { SSSData } from '../Model/SSS-data.const';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatButtonModule,MatInputModule,CdkAccordionModule],
+  imports: [MatButtonModule,MatInputModule,CdkAccordionModule,FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
